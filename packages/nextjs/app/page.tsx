570bae596bdcb3eb92b7e6f5257dbbeff4266c4b
gabref <gabrielfranzer@gmail.com>
2024-02-17 19:09:19 +0100

    style: changed tailwind styles

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,19 +1,19 @@
 "use client";
 
-import Link from "next/link";
 import type { NextPage } from "next";
-import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { Address } from "~~/components/scaffold-eth";
 import { ForwardPageButton } from "~~/components/ForwardPageButton";
 
 const Home: NextPage = () => {
-  return (
-    <>
-		<ForwardPageButton to="/customer" innerText="Customer" />
-		<ForwardPageButton to="/checkpoint" innerText="Check Point" />
-    </>
-  );
+	return (
+		<div className="container mx-auto px-4 py-8">
+			<h1 className="text-3xl font-bold mb-8 text-center">Welcome to Your Application</h1>
+			<div className="flex flex-col space-y-4 items-center">
+				<ForwardPageButton to="/customer" innerText="Customer" />
+				<ForwardPageButton to="/checkpoint" innerText="Check Point" />
+				<ForwardPageButton to="/admin" innerText="Admin" />
+			</div>
+		</div>
+	);
 };
 
 export default Home;
